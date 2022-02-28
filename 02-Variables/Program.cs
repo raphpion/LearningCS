0e4c037f0647b639b66695862a26c61f7cdd9431
Raphaël Pion <raph.pion@gmail.com>
2022-02-28 11:01:49 -0500

    Resolve chap.3

---
+++ 02-Variables/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _02_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 24;
+            string prenom = "Raphaël";
+            Console.WriteLine(prenom + ", " + age + " ans");
+        }
+    }
+}
---
+++ 03-Conditions/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _03_Conditions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal accBalance = 300;
+            bool estCrediteur = accBalance >= 0;
+            if (estCrediteur)
+                Console.WriteLine("Votre compte est créditeur");
+            else
+                Console.WriteLine("Votre compte est débiteur");
+        }
+    }
+}
